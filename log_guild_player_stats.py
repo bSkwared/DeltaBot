09ac2d746a75a96de9a166e694012a476018f0de
bSkwared <laskyblake@gmail.com>
2022-12-09 22:07:13 -0600

    Rename main file

--- log_guild_player_stats.py
+++ log_guild_player_stats.py
@@ -1,4 +1,8 @@
 from api_swgoh_help import api_swgoh_help
+import json
+from datetime import datetime, timezone
+import ntplib
+
 
 # Change the settings below
 with open('./ally_passwd.txt') as fd:
@@ -29,10 +33,21 @@ def getGuildAllycodes(guild_dict):
             return ([])
     return allycodes
 
+players = {}
+
+c = ntplib.NTPClient()
+r = c.request('time.google.com')
+players = {'1': {}, '2': {}}
+log_obj = {}
+log_obj['timestamp'] = datetime.fromtimestamp(r.tx_time, timezone.utc).strftime("%m_%d_%Y_%H:%M")
+log_obj['players'] = players
+
+print(json.dumps(log_obj, indent=2))
+
 
 # Fetch a list of guild member allycodes
 members = getGuild(client, allycode)
 g_allycodes = getGuildAllycodes(members)
 print(g_allycodes)
-#import json
+
 #print(json.dumps(client.fetchPlayers(g_allycodes), indent=2))
