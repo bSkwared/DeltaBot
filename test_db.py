ad39d4cd88a05c99fd4d35f0634f8186462bc74a
bSkwared <laskyblake@gmail.com>
2022-12-14 00:27:39 -0600

    Add initial classes and test db creation/insertion

--- test_db.py
+++ test_db.py
@@ -1,28 +1,38 @@
 import peewee as PW
+import ntplib
+import datetime
+import json
+import os
 import datetime
 
 db = PW.SqliteDatabase('dev.db')
 
-class Guild(PW.Model):
+class BaseModel(PW.Model):
+    class Meta:
+        database = db
+
+class Guild(BaseModel):
+    ref_id = PW.CharField(primary_key=True)
     name = PW.CharField()
 
-class Player(PW.Model):
+class Player(BaseModel):
     ally_code = PW.IntegerField(unique=True)
     name = PW.CharField()
 
-class StatsDump(PW.Model):
-    timestamp = DateTimeField()
+class StatsDump(BaseModel):
+    timestamp = PW.DateTimeField()
     player = PW.ForeignKeyField(Player)
     guild = PW.ForeignKeyField(Guild)
 
-class Toon(PW.Model):
+class Toon(BaseModel):
+    stats_dump = PW.ForeignKeyField(StatsDump)
     gp = PW.IntegerField()
     stars = PW.IntegerField()
     gear_level = PW.IntegerField()
     relic_level = PW.IntegerField()
     gear_slots_filled = PW.IntegerField()
 
-class Mod(PW.Model):
+class Mod(BaseModel):
     shape = PW.IntegerField()
     mod_set = PW.IntegerField()
     dots = PW.IntegerField()
@@ -31,7 +41,34 @@ class Mod(PW.Model):
     primary_stat = PW.CharField()
     primary_value = PW.CharField()
 
-class SecondaryMod(PW.Model):
+class SecondaryStat(BaseModel):
     stat = PW.CharField()
     value = PW.CharField()
     num_rolls = PW.IntegerField()
+
+db.create_tables([Guild, Player, StatsDump, Toon, Mod, SecondaryStat])
+#Player(ally_code=917787877, name='RedFox').save()
+
+player_dump = {}
+with open('guild_player_stats.json', 'r') as fp:
+    player_dump = json.load(fp)
+
+
+c = ntplib.NTPClient()
+r = c.request('time.google.com')
+log_time = datetime.datetime.fromtimestamp(r.tx_time, datetime.timezone.utc)
+
+for player in player_dump:
+    #breakpoint()
+    p, _ = Player.get_or_create(ally_code=player['allyCode'], name=player['name'])
+
+    sd = StatsDump(guild=Guild(ref_id=player['guildRefId']), timestamp=log_time, player=p)
+    sd.save()
+
+    for unit in player['roster']:
+        t = Toon(stats_dump=sd, stars=unit['rarity'], gear_level=unit['gear'], relic_level=unit['relic']['currentTier']
+
+
+
+
+    
