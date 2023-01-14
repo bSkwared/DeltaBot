63848e2c1b302a1bffe64706191082f6fad1aa87
bSkwared <laskyblake@gmail.com>
2023-01-14 01:33:18 +0000

    Move auth to a python file, and random updates to the dicord bot

--- bin/discord_bot.py
+++ bin/discord_bot.py
@@ -6,6 +6,7 @@ import config.config as config
 import disnake
 import json
 from swgoh_comlink import SwgohComlink
+import config.auth
 
 import logging
 
@@ -20,19 +21,19 @@ guru_log.add("/tmp/deltabot.log", backtrace=True, diagnose=True)
 with open(config.AUTH_CONFIG, 'r') as fp:
     auth = json.load(fp)
 
-BYPASS_GEAR_LEVEL = True
+BYPASS_GEAR_LEVEL = False
 comlink = SwgohComlink(url='http://localhost:3200')
 player_data = comlink.get_player(917787877)
 player_data['name']
 guild_id = player_data['guildId']
 DELTABOT_TEST = 1062981461227077694
 BS_TEST = 1062980772736286740
-cur_chan = DELTABOT_TEST
 cur_chan = BS_TEST
+cur_chan = DELTABOT_TEST
 GLOBAL_PATH = '/home/server/source/DeltaBot/tmp/delta.json'
 TMP_GLOBAL_PATH = f'{GLOBAL_PATH}.tmp'
 
-GLOBAL = {'guild': [], 'players': {}, 'last_players': {}, 'unit_id_to_name' : {}}
+GLOBAL = {'guild': [], 'players': {}, 'last_players': {}, 'unit_id_to_name' : {}, 'updates': {}, 'cur_seq': 0}
 def log(msg):
     guru_log.info(f'{datetime.datetime.now()}: {msg}')
 
@@ -73,24 +74,38 @@ def update_unit_id_to_name():
 def update_guild():
     GLOBAL['guild'] = comlink.get_guild(guild_id)
 
+def get_player_data(pID):
+    player = comlink.get_player(player_id=pID)
+    roster = []
+    if 'rosterUnit' not in player:
+        try:
+            log(f'blake keys: {player.keys()}')
+        except:
+            log(f'blake player: {player}')
+    for c in player['rosterUnit']:
+        toon = {k: c[k] for k in ('id', 'relic', 'definitionId', 'currentRarity', 'currentTier')}
+        roster.append(toon)
+
+    return {
+            'name': player['name'],
+            'rosterUnit': roster,
+            'playerId': player['playerId']
+    }
+
 def update_players():
     players = {}
     for p in GLOBAL['guild']['member']:
-        player = comlink.get_player(player_id=p['playerId'])
-        roster = []
-        for c in player['rosterUnit']:
-            toon = {k: c[k] for k in ('id', 'relic', 'definitionId', 'currentRarity', 'currentTier')}
-            roster.append(toon)
+        for _ in range(3):
+            try:
+                players[p['playerId']] = get_player_data(p['playerId'])
+                break
+            except Exception as e:
+                log(f'blake breaking {e}')
+                pass
 
-        player_data = {
-                'name': player['name'],
-                'rosterUnit': roster,
-                'playerId': p['playerId']
-        }
-
-        players[p['playerId']] = player_data
     #GLOBAL['players'] = {p['playerId']: comlink.get_player(player_id=p['playerId']) for p in GLOBAL['guild']['member']}
     GLOBAL['players'] = players
+
 
 class MyClient(disnake.Client):
     async def on_ready(self):
@@ -98,16 +113,16 @@ class MyClient(disnake.Client):
         channel = self.get_channel(cur_chan)
         if not isinstance(channel, disnake.TextChannel):
             raise ValueError("Invalid channel")
-        await channel.send("Bot starting")
+        #await channel.send("Bot starting")
 
         try:
             loop = asyncio.get_event_loop()
 
             last_players = {}
             players = {}
-            cur_seq = 0
+            GLOBAL['cur_seq'] = 0
             last_updated_names = -100
-            player_updates = {}
+            GLOBAL['player_updates'] = {}
             # pID: {
             #     'last_updated': cur_seq,
             #     'toons': {
@@ -119,7 +134,7 @@ class MyClient(disnake.Client):
             #     },
             # }
             while True:
-                cur_seq += 1
+                GLOBAL['cur_seq'] += 1
                 log('Poll for new update to guild and players')
                 await loop.run_in_executor(None, update_guild)
                 await loop.run_in_executor(None, update_players)
@@ -137,9 +152,9 @@ class MyClient(disnake.Client):
                     for c in np['rosterUnit']:
                         lc = lp_roster.get(c['id'])
                         d_split = c["definitionId"].split(":")[0]
-                        if d_split not in GLOBAL['unit_id_to_name'] and last_updated_names + 100 < cur_seq:
+                        if d_split not in GLOBAL['unit_id_to_name'] and last_updated_names + 100 < GLOBAL['cur_seq']:
                             log(f'Updating names since {d_split} not found in mapping')
-                            last_updated_names = cur_seq
+                            last_updated_names = GLOBAL['cur_seq']
                             await loop.run_in_executor(None, update_unit_id_to_name)
                         unit_name = GLOBAL['unit_id_to_name'].get(d_split, d_split)
 
@@ -149,8 +164,8 @@ class MyClient(disnake.Client):
                             or (c.get('relic') and lc.get('relic')
                                 and lc['relic']['currentTier'] != c['relic']['currentTier'])):
 
-                            update = player_updates.get(npID, {})
-                            update['last_updated'] = cur_seq
+                            update = GLOBAL['player_updates'].get(npID, {})
+                            update['last_updated'] = GLOBAL['cur_seq']
                             toons = update.get('toons', {})
                             unit = toons.get(unit_name, {})
                             unit['initial_stars'] = min(lc['currentRarity'] if lc else 0,
@@ -173,74 +188,48 @@ class MyClient(disnake.Client):
                                                            else 0)
                             toons[unit_name] = unit
                             update['toons'] = toons
-                            player_updates[npID] = update
+                            GLOBAL['player_updates'][npID] = update
 
 
 
                 deleted_keys = []
-                for pID, update in player_updates.items():
-                    if update['last_updated'] + 3 > cur_seq:
+                for pID, update in GLOBAL['player_updates'].items():
+                    if update['last_updated'] + 3 > GLOBAL['cur_seq']:
                         continue
 
                     deleted_keys.append(pID)
-                    msg = []
+                    pmsg = []
                     for name, stats in update['toons'].items():
+                        msg = []
                         if stats['initial_stars'] == 0:
-                            msg.append(f'Unlocked {unit_name} at {stats["latest_stars"]} stars')
+                            msg.append(f'Unlocked {name} at {stats["latest_stars"]} stars')
                             if stats['latest_gear'] > 1:
                                 msg.append(f'Gear level: {stats["latest_gear"]}')
                             if stats['latest_relic'] > 1:
                                 msg.append(f'Relic level: {stats["latest_relic"]}')
+                            pmsg.append('\n'.join(msg))
                         else:
-                            msg.append(name)
-                            if stats['initial_stars'] != stats['latest_stars']:
+                            if stats['initial_stars'] != stats['latest_stars'] and stats['latest_stars'] == 7:
                                 msg.append(f'Stars: {stats["initial_stars"]} -> {stats["latest_stars"]}')
-                            if stats['initial_gear'] != stats['latest_gear']:
+                            if stats['initial_gear'] != stats['latest_gear'] and stats['latest_gear'] >= 12:
                                 msg.append(f'Gear: {stats["initial_gear"]} -> {stats["latest_gear"]}')
                             if stats['initial_relic'] != stats['latest_relic']:
                                 msg.append(f'Relic: {stats["initial_relic"]} -> {stats["latest_relic"]}')
+                            if msg:
+                                msg = [name] + msg
+                                pmsg.append('\n'.join(msg))
 
-                    if msg:
-                        pmsg = '\n\n'.join(msg)
-                        update_msg = f'**{np["name"]}**\n{pmsg}'
+                    if pmsg:
+                        pmsg = '\n\n'.join(pmsg)
+                        player_name = GLOBAL["players"].get(pID, {}).get("name", "UNKNOWN")
+                        update_msg = f'**{player_name}**\n{pmsg}'
                         log(f'Attempting to send udate message {update_msg}')
-                        await channel.send(update_msg)
+                        embed = disnake.Embed(description=pmsg, colour=0x801010)
+                        embed.set_author(name=player_name)
+                        await channel.send(embed=embed)
 
-                    '''
-
-                    if not lc:
-                            msg.append(f'Unlocked {unit_name} at {c["currentRarity"]} stars')
-                            continue
-
-                        c_msg = []
-
-                        if c['currentRarity'] != lc['currentRarity']:
-                            c_msg.append(f'Stars: {lc["currentRarity"]} -> {c["currentRarity"]}')
-
-                        if c['currentTier'] != lc['currentTier']:
-                            if c['currentTier'] >= 12 or BYPASS_GEAR_LEVEL:
-                                c_msg.append(f'Gear level: {lc["currentTier"]} -> {c["currentTier"]}')
-
-                        if c.get('relic') and lc.get('relic'):
-                            lr = max(0, lc['relic']['currentTier'] - 2)
-                            cr = max(0, c['relic']['currentTier'] - 2)
-                            if lr != cr:
-                                c_msg.append(f'Relic level: {lr} -> {cr}')
-
-                        if c_msg:
-                            msg.append(unit_name)
-                            msg.extend(c_msg)
-                            msg.append('')
-
-
-                    if msg:
-                        pmsg = '\n'.join(msg)
-                        update_msg = f'**{np["name"]}**\n{pmsg}'
-                        log(f'Attempting to send udate message {update_msg}')
-                        await channel.send(update_msg)
-                    '''
                 for k in deleted_keys:
-                    del player_updates[k]
+                    del GLOBAL['player_updates'][k]
 
                 GLOBAL['last_players'].update(GLOBAL['players'])
                 try:
@@ -254,12 +243,13 @@ class MyClient(disnake.Client):
                 log('Begin sleeping for 30s')
                 await asyncio.sleep(30)
         finally:
-            await channel.send("Bot stopping")
+            pass
+            #await channel.send("Bot stopping")
 
 
     async def on_message(self, message):
         print(f'Message from {message.author}: {message.content}')
 
 client = MyClient()
-client.run(auth['discord_token'])
+client.run(config.auth.discord_token)
 
