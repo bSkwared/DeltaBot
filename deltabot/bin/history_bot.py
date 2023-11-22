a8d672cb33436ae47662baa21fc7401e79cef62b
bSkwared <laskyblake@gmail.com>
2023-11-22 22:48:54 +0000

    Update bot to have better labels, accept guild_combined, and multiple allies/users

--- deltabot/bin/history_bot.py
+++ deltabot/bin/history_bot.py
@@ -17,7 +17,7 @@ import datetime
 
 bot = commands.Bot()
 
-'''
+allycodes_to_ids = '''
 797668393 : 0 : <@165166478490861568>
 168232195 : 1 : <@548367852587974669>
 813135746 : 0 : <@526049061015519234>
@@ -67,120 +67,14 @@ bot = commands.Bot()
 769144997 : 1 : <@81650192071262208>
 '''
 
-allycode_mappings = '''
-797668393 : @acekyrios
-168232195 : @apexjoker
-245638798 : @bradley#9115
-842925842 : @camman113
-969845633 : @i.chewy
-168688624 : @clerical_errors
-126483852 : @curlymechanic
-467499596 : @_dankobah_
-278342864 : @darkhope17
-825981817 : @darthsauron77
-138959414 : @el_dudarino
-231914963 : @estefanwolf
-369472317 : @explxsive
-937291233 : @rcracingcar
-151593975 : @floki_ver_verlander
-662511953 : @fnodo
-648313163 : @foxesgaming
-629268449 : @francescoo7
-262722744 : @hawkeye___
-738679812 : @Hank#5350
-785691717 : @immortal8336
-257115149 : @kratos16
-938893196 : @martymcfly2.0
-243453117 : @fwylo
-924957122 : @Kibar#8834
-892738564 : @morbidpie
-648316781 : @mowmow17
-716896212 : @overlord1545
-841413243 : @nocturnokvlto
-846156634 : @northern_adam#8701
-538994795 : @sjay#8131
-637384578 : @rslimshark
-917787877 : @bskwared
-676728824 : @Renegade7976#9848
-869469465 : @roklar
-326458668 : @sapphirefox1515
-367251355 : @sergeantpickle
-413742635 : @halis360
-992228328 : @danielwg008
-849672945 : @unicorncakes
-972981788 : @xraus_pete
-153822847 : @zyphon7
-847818777 : @bestbudben
-351352931 : @brothersofgoodandevil
-667937846 : @ddtorch
-874296875 : @shaneo97#4561
-769144997 : @stryderjzw
-'''
-
-user_to_ally = {}
-for line in allycode_mappings.splitlines():
+id_to_ally = {}
+for line in allycodes_to_ids.splitlines():
     if not line:
         continue
 
-    allycode, username = line.split(':')
-    allycode = allycode.strip()
-    username = username.strip()
-    # remove @
-    username = username[1:]
-    user_to_ally[username] = allycode
+    allycode, _, ID = line.split(' : ')
+    id_to_ally[int(ID[2:-1])] = allycode
 
-
-
-def name_tag_combine(name, tag):
-    username = name
-    if tag and tag != '0':
-        username += f'#{tag}'
-    return username
-
-
-
-'''
-end = WD.get_current_datetime()
-start = end - datetime.timedelta(days=args.duration)
-
-gp_names = ['redfox', 'sapp']
-gp_diff = [100, 200]
-gp_perc = [0.1, 0.2]
-
-fig = px.line({'Date': dates, 'GP': gps}, x='Date', y='GP')
-
-
-fig = px.bar({'Name': gp_names, 'Growth': gp_diff}, x='Name', y='Growth')
-fig.update_xaxes(dtick=1)
-fig.update_layout(barmode='stack', xaxis={'categoryorder':'total ascending'})
-fig.write_image("tmp/fig1.png")
-
-fig = px.bar({'Name': gp_names, 'Growth': gp_perc}, x='Name', y='Growth')
-fig.update_xaxes(dtick=1)
-fig.update_layout(barmode='stack', xaxis={'categoryorder':'total ascending'})
-fig.write_image("tmp/fig2.png")
-print(len(gp_names))
-'''
-
-def modscore(speed_mods, char_gp):
-    return raw_mod_value(speed_mods) / (char_gp * 3 / 100_000)
-
-def get_subject_allycode(author, allycode, user):
-    if allycode and user:
-        raise Exception('Please provide only an allycode or a user (or neither)')
-
-    subject_allycode = None
-    if allycode:
-        if len(allycode) != 9 or not allycode.isdigit():
-            raise Exception(f'invalid allycode (should be 9 digits): {allycode}')
-        return allycode
-
-    else:
-        author_username = name_tag_combine(author.name, author.tag)
-        subject_username = author_username if not user else name_tag_combine(user.name, user.tag)
-        if subject_username not in user_to_ally:
-            raise Exception(f'Unable to find allycode for {author_username}')
-        return user_to_ally[subject_username]
 
 
 def load_gp(daily_data, allycode, guild_combined):
@@ -201,31 +95,49 @@ def load_modscore(daily_data, allycode, guild_combined):
 
 
 def load_raw_mods(daily_data, allycode, guild_combined):
-    individual_data = daily_data.get('guild_members', {}).get(allycode, {})
-    if 'speed_mods' not in individual_data:
-        return None
+    total = None
+    for ac, m in daily_data.get('guild_members', {}).items():
+        if guild_combined or ac == allycode:
+            if 'speed_mods' not in m:
+                continue
+            total = total or 0
+            for speed, count in enumerate(m['speed_mods']):
+                if speed <= 10:
+                    continue
 
-    total = 0
-    for speed, count in enumerate(individual_data['speed_mods']):
-        if speed <= 10:
-            continue
+                speed_value = (  -6.3058820272303500e+000 * (speed**0)
+                               +  1.3035991984201347e+000 * (speed**1)
+                               + -9.6654093848707642e-002 * (speed**2)
+                               +  2.7728738967038821e-003 * (speed**3))
 
-        speed_value = (  -6.3058820272303500e+000 * (speed**0)
-                       +  1.3035991984201347e+000 * (speed**1)
-                       + -9.6654093848707642e-002 * (speed**2)
-                       +  2.7728738967038821e-003 * (speed**3))
-
-        total += count * speed_value
+                total += count * speed_value
 
     return total
 
 
+
 def load_char_gp(daily_data, allycode, guild_combined):
-    return daily_data.get('guild_members', {}).get(allycode, {}).get('char_gp')
+    members = daily_data.get('guild_members', {})
+    total = None
+    for ac, m in daily_data.get('guild_members', {}).items():
+        if guild_combined or ac == allycode:
+            if 'char_gp' not in m:
+                continue
+            total = total or 0
+            total += m.get('char_gp', 0)
+    return total
 
 
 def load_ship_gp(daily_data, allycode, guild_combined):
-    return daily_data.get('guild_members', {}).get(allycode, {}).get('ship_gp')
+    members = daily_data.get('guild_members', {})
+    total = None
+    for ac, m in daily_data.get('guild_members', {}).items():
+        if 'ship_gp' not in m:
+            continue
+        if guild_combined or ac == allycode:
+            total = total or 0
+            total += m.get('ship_gp', 0)
+    return total
 
 
 def load_mods_15_speed(daily_data, allycode, guild_combined):
@@ -237,10 +149,14 @@ def load_mods_20_speed(daily_data, allycode, guild_combined):
 
 
 def count_mods_speed_threshold(daily_data, allycode, threshold, guild_combined):
-    individual_data = daily_data.get('guild_members', {}).get(allycode, {})
-    if 'speed_mods' not in individual_data:
-        return None
-    return sum(individual_data['speed_mods'][threshold:])
+    total = None
+    for ac, m in daily_data.get('guild_members', {}).items():
+        if guild_combined or ac == allycode:
+            if 'speed_mods' not in m:
+                continue
+            total = total or 0
+            total += sum(m.get('speed_mods', [])[threshold:])
+    return total
     
 
 
@@ -286,24 +202,48 @@ async def progression(
         stat: str = commands.Param(autocomplete=autocomp_stats, description='What stat to show progression in'),
         days_back: int = commands.Param(description='How many days to go back in history', default=90, le=1000),
         guild_combined: bool = commands.Param(description='UNUSED', default=False),
-        allycode: str = commands.param(description='Defaults to who ran this command', default=None),
-        user: disnake.User = commands.param(description='Defaults to who ran this command', default=None),
-        ally1: str = commands.param(description='UNUSED - Additional allycode to chart', default=None),
-        ally2: str = commands.param(description='UNUSED - Additional allycode to chart', default=None),
-        ally3: str = commands.param(description='UNUSED - Additional allycode to chart', default=None),
-        ally4: str = commands.param(description='UNUSED - Additional allycode to chart', default=None),
-        user1: disnake.User = commands.param(description='UNUSED - Additional user to chart', default=None),
-        user2: disnake.User = commands.param(description='UNUSED - Additional user to chart', default=None),
-        user3: disnake.User = commands.param(description='UNUSED - Additional user to chart', default=None),
-        user4: disnake.User = commands.param(description='UNUSED - Additional user to chart', default=None),
+        hide_self: bool = commands.Param(description='Include yourself with other users', default=False),
+        user1: disnake.User = commands.param(description='Additional user to chart', default=None),
+        user2: disnake.User = commands.param(description='Additional user to chart', default=None),
+        user3: disnake.User = commands.param(description='Additional user to chart', default=None),
+        user4: disnake.User = commands.param(description='Additional user to chart', default=None),
+        ally1: str = commands.param(description='Additional allycode to chart', default=None),
+        ally2: str = commands.param(description='Additional allycode to chart', default=None),
+        ally3: str = commands.param(description='Additional allycode to chart', default=None),
+        ally4: str = commands.param(description='Additional allycode to chart', default=None),
         ):
-    try:
-        subject_allycode = get_subject_allycode(inter.author, allycode, user)
-    except Exception as e:
-        await inter.response.send_message(str(e))
-        return
 
-    subject_allycodes = [subject_allycode] 
+    subject_allycodes = []
+    err_msg = []
+    def add_users_ac(user):
+        if user.id not in id_to_ally:
+            err_msg.append(f'Unable to find allycode for {user.name}')
+            return
+        subject_allycodes.append(id_to_ally[user.id])
+
+    def add_ac(ac):
+        if len(ac) != 9 or not ac.isdigit():
+            err_msg.append(f'invalid allycode (should be 9 digits): {ac}')
+            return
+        subject_allycodes.append(ac)
+
+    for u in (None if hide_self or guild_combined else inter.author, user1, user2, user3, user4):
+        if u:
+            add_users_ac(u)
+
+    for ac in (ally1, ally2, ally3, ally4):
+        if ac:
+            add_ac(ac)
+
+    if not guild_combined and not subject_allycodes:
+        err_msg.append(f'Please provide users or allycodes')
+
+    if guild_combined and subject_allycodes:
+        err_msg.append(f'Please do not add users or allycodes for combined data')
+
+    if err_msg:
+        await inter.response.send_message('\n'.join(err_msg))
+        return
 
     await inter.response.defer()
     cur_stat = STATS[stat]
@@ -315,10 +255,15 @@ async def progression(
         cur_date += datetime.timedelta(days=1)
 
 
-
+    GUILD_FAKE_AC = 'CCD123'
+    GUILD_NAME = 'CCD'
     ac_data = {}
     ac_latest_name = {}
-    ac_last_name_date = {}
+    if guild_combined:
+        assert not subject_allycodes
+        subject_allycodes.append(GUILD_FAKE_AC)
+        ac_latest_name[GUILD_FAKE_AC] = GUILD_NAME
+
     for cur_date in days:
         filename = cur_date.strftime("%Y_%m_%d.py")
         try:
@@ -331,22 +276,15 @@ async def progression(
             if sac not in ac_data:
                 ac_data[sac] = []
             ac_data[sac].append(cur_stat['load_func'](guild_data, sac, guild_combined))
-
-        cur_name  = guild_data['guild_members'].get(sac, {}).get('name')
-        print(cur_name)
-        cur_name_newer = ac_last_name_date.get(sac) == None or cur_date > ac_last_name_date.get(sac)
-        if cur_name and cur_name_newer:
-            ac_latest_name[sac] = cur_name
-            ac_last_name_date[sac] = cur_date
+            member = guild_data['guild_members'].get(sac, {})
+            if 'name' in member:
+                ac_latest_name[sac] = member['name'].replace(' ', '')[:10]
 
     if not ac_data:
         await inter.followup.send('unable to find data')
         return
 
     
-    print(days)
-    print(ac_data)
-    print(ac_latest_name)
     plot_data = {'Date': days}
     names = []
     for ac, gps in ac_data.items():
@@ -354,10 +292,9 @@ async def progression(
         names.append(ac_latest_name[ac])
 
     df = pd.DataFrame.from_dict(plot_data)
-    print(df)
-    print(f'columns: {df.columns[1]}')
-    print(f'columns: {df.columns}')
-    fig = px.line(df, x='Date', y=df.columns[1:], template='plotly_dark')
+    fig = px.line(df, x='Date', y=df.columns[1:], template='plotly_dark',
+            title=f'{cur_stat["ui_name"]} for {", ".join(names)} last {days_back} days',
+            labels={'variable':'Player', 'value':cur_stat['ui_name']})
     # fig.update_yaxes(rangemode="tozero")
     fig_filepath = os.path.join(config.TMP_DIR, f'{stat}_since_{days_back}_{inter.author.name}.png')
     fig.write_image(fig_filepath)
